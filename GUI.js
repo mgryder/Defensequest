cba9b6f0f1cef1fa80d4e12f8283bb8d4f26daa1
Martin Gryder <54819190+mgryder@users.noreply.github.com>
2024-11-20 17:54:52 -0500

    Looped the counter

--- GUI.js
+++ GUI.js
@@ -1,5 +1,10 @@
-var turn=0;
+var turn=1;
 function endTurn(){
-    turn++;
+    if(turn>32){
+        turn=1;
+    }
+    else{
+        turn++;
+    }
     document.getElementById("testCounter").innerHTML=turn;
 }
